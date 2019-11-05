4282c4bc7fb6c9d1dcf626cfc9a51de21e87d52d
Axel Perroud <ape@emakina.fr>
2019-11-05 23:02:41 +0100

    Reformat and correct release.sh & add success message when application is updated

--- release.sh
+++ release.sh
@@ -3,41 +3,56 @@
 set -e
 
 if [ $# -ne 1 ]; then
-  echo "Usage: `basename $0` <tag>"
-  exit 65
+    echo "Usage: $(basename $0) <tag>"
+    exit 65
 fi
-
 
 # CHECK MASTER BRANCH
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [[ "$CURRENT_BRANCH" != "master" ]]; then
-  echo "You have to be on master branch currently on $CURRENT_BRANCH . Aborting"
-  exit 65
+    echo "You have to be on master branch currently on $CURRENT_BRANCH . Aborting"
+    exit 65
 fi
 
 # CHECK FORMAT OF THE TAG
 php -r "if(preg_match('/^\d+\.\d+\.\d+(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?\$/',\$argv[1])) exit(0) ;else{ echo 'format of version tag is not invalid' . PHP_EOL ; exit(1);}" $1
 
-
 # CHECK jsawk COMMAND
-command -v jsawk >/dev/null 2>&1 || { echo "Error : Command jsawk is not installed on the system"; echo "See : https://github.com/micha/jsawk "; echo  "Exiting..." >&2; exit 65; }
+command -v jsawk >/dev/null 2>&1 || {
+    echo "Error : Command jsawk is not installed on the system"
+    echo "See : https://github.com/micha/jsawk "
+    echo "Exiting..." >&2
+    exit 65
+}
 
 # CHECK js COMMAND
-command -v js >/dev/null 2>&1 || { echo "Error : Command js is not installed on the system"; echo "Should be fixed by installing spidermonkey "; echo  "Exiting..." >&2; exit 65; }
+command -v js >/dev/null 2>&1 || {
+    echo "Error : Command js is not installed on the system"
+    echo "Should be fixed by installing spidermonkey "
+    echo "Exiting..." >&2
+    exit 65
+}
 
 # CHECK box COMMAND
-command -v box >/dev/null 2>&1 || { echo "Error : Command box is not installed on the system"; echo "See : https://github.com/box-project/box2 "; echo  "Exiting..." >&2; exit 65; }
+command -v box >/dev/null 2>&1 || {
+    echo "Error : Command box is not installed on the system"
+    echo "See : https://github.com/box-project/box2 "
+    echo "Exiting..." >&2
+    exit 65
+}
 
 # CHECK python COMMAND
-command -v python >/dev/null 2>&1 || { echo "Error : Command python is not installed on the system"; echo  "Exiting..." >&2; exit 65; }
+command -v python >/dev/null 2>&1 || {
+    echo "Error : Command python is not installed on the system"
+    echo "Exiting..." >&2
+    exit 65
+}
 
 # CHECK THAT WE CAN CHANGE BRANCH
 git checkout gh-pages
 git checkout --quiet master
 
-
 TAG=$1
-
 
 #
 # Tag & build master branch
@@ -64,7 +79,7 @@ JSON="${JSON},version:\"${TAG}\""
 #
 # Update manifest
 #
-cat manifest.json | jsawk -a "this.push({${JSON}})" | python -mjson.tool > manifest.json.tmp
+cat manifest.json | jsawk -a "this.push({${JSON}})" | python -mjson.tool >manifest.json.tmp
 mv manifest.json.tmp manifest.json
 git add manifest.json
 
@@ -77,4 +92,4 @@ git checkout master
 
 echo "New version created. Now you should run:"
 echo "git push origin gh-pages"
-echo "git push ${TAG}"
+echo "git push origin ${TAG}"
--- src/Command/UpdateCommand.php
+++ src/Command/UpdateCommand.php
@@ -5,9 +5,11 @@ namespace Magephi\Command;
 use Herrera\Phar\Update\Manager;
 use Herrera\Phar\Update\Manifest;
 use Magephi\Application;
+use Magephi\Kernel;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 /**
  * Command to upgrade the application to the latest version.
@@ -16,27 +18,47 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class UpdateCommand extends Command
 {
-    const MANIFEST_FILE = 'https://fulmenef.github.io/magephi/manifest.json';
+	const MANIFEST_FILE = 'https://fulmenef.github.io/magephi/manifest.json';
 
-    protected function configure()
-    {
-        $this
-            ->setName('magphi:update')
-            ->setAliases(['update'])
-            ->setDescription('Updates Magphi to the latest version')
-        ;
-    }
+	/** @var SymfonyStyle */
+	private $interactive;
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $app = $this->getApplication();
-        if ($app instanceof Application) {
-            $manager = new Manager(Manifest::loadFile(self::MANIFEST_FILE));
-            $manager->update($app->getVersion(), true);
-        } else {
-            throw new \Exception(
-                sprintf('Application must be type of %s, %s found.', Application::class, \gettype($app))
-            );
-        }
-    }
+	protected function configure()
+	{
+		$this
+			->setName('magphi:update')
+			->setAliases(['update'])
+			->setDescription('Updates Magphi to the latest version');
+	}
+
+	protected function initialize(InputInterface $input, OutputInterface $output)
+	{
+		$this->interactive = new SymfonyStyle($input, $output);
+		parent::initialize($input, $output);
+	}
+
+	/**
+	 * TODO: Ask before update and display new version at the end
+	 * TODO: Double check when a major version is released and the user accept to upgrade
+	 *
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 *
+	 * @return int|void|null
+	 * @throws \Exception
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$app = $this->getApplication();
+		if ($app instanceof Application) {
+			$manager = new Manager(Manifest::loadFile(self::MANIFEST_FILE));
+			$manager->update($app->getVersion(), true);
+
+			$this->interactive->success(sprintf("%s has been upgraded to the latest version", Kernel::NAME));
+		} else {
+			throw new \Exception(
+				sprintf('Application must be type of %s, %s found.', Application::class, \gettype($app))
+			);
+		}
+	}
 }
