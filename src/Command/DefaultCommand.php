81e1294d5c13e1a1b879b10c4410846d7a874514
Axel Perroud <ape@emakina.fr>
2019-11-17 00:05:05 +0100

    Add new version check on the default command

--- src/Command/DefaultCommand.php
+++ src/Command/DefaultCommand.php
@@ -4,12 +4,11 @@ namespace Magephi\Command;
 
 use Exception;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class DefaultCommand extends Command
+class DefaultCommand extends AbstractCommand
 {
     /**
      * {@inheritdoc}
