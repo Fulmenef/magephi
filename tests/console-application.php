e57db45e81ce05de958cdbfedec507f5bbcfae2f
Axel Perroud <ape@emakina.fr>
2021-10-24 00:30:33 +0200

    Remove source files

--- tests/console-application.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__ . '/../config/bootstrap.php';
-
-$kernel = new \Magephi\Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-
-return new \Magephi\Application($kernel);
